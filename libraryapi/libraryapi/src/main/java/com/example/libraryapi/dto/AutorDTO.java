929e3f31de84d3f38256897c59ff3d2e49865685
brunohuguenin <brunohugueninn@gmail.com>
2025-08-23 13:18:34 -0300

    Adicionando validação de campos com Bean Validation

--- libraryapi/libraryapi/src/main/java/com/example/libraryapi/dto/AutorDTO.java
+++ libraryapi/libraryapi/src/main/java/com/example/libraryapi/dto/AutorDTO.java
@@ -1,14 +1,19 @@
 package com.example.libraryapi.dto;
 
 import com.example.libraryapi.model.Autor;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.UUID;
 
 public record AutorDTO(
         UUID id,
+        @NotBlank(message = "Campo obrigatório")
         String nome,
+        @NotNull(message = "Campo obrigatório")
         LocalDate dataNascimento,
+        @NotBlank(message = "Campo obrigatório")
         String nacionalidade
 
 ) {
