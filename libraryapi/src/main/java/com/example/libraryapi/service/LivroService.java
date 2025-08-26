2f4595051bde21e9c7faa9c792ef5debf8c4d720
brunohuguenin <brunohugueninn@gmail.com>
2025-08-26 16:32:33 -0300

    Implementando as Specifications da entidade livro

--- libraryapi/src/main/java/com/example/libraryapi/service/LivroService.java
+++ libraryapi/src/main/java/com/example/libraryapi/service/LivroService.java
@@ -1,11 +1,16 @@
 package com.example.libraryapi.service;
 
 
+import com.example.libraryapi.model.GeneroLivro;
 import com.example.libraryapi.model.Livro;
 import com.example.libraryapi.repository.LivroRepository;
+import com.example.libraryapi.repository.specs.LivroSpecs;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import static com.example.libraryapi.repository.specs.LivroSpecs.*;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -26,4 +31,30 @@ public class LivroService {
     public void deletar(Livro livro) {
         livroRepository.delete(livro);
     }
+
+    public List<Livro> pesquisa(
+            String isbn, String titulo, String nomeAutor, GeneroLivro generoLivro, Integer anoPublicacao
+    ) {
+//        Specification<Livro> specs = Specification
+//                .where(LivroSpecs.isbnEqual(isbn))
+//                .and(LivroSpecs.tituloLike(titulo))
+//                .and(LivroSpecs.generoEqual(generoLivro));
+
+        Specification<Livro> specs = Specification.where((root, query, cb) -> cb.conjunction());
+
+        if (isbn != null) {
+            specs = specs.and(isbnEqual(isbn));
+        }
+
+        if (titulo != null) {
+            specs = specs.and(tituloLike(titulo));
+        }
+
+        if (generoLivro != null) {
+            specs = specs.and(generoEqual(generoLivro));
+        }
+
+        return livroRepository.findAll(specs);
+    }
+
 }
