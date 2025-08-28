1a8b2ec93d536b31ca689bd20b0c43ffe3729faa
brunohuguenin <brunohugueninn@gmail.com>
2025-08-28 13:12:33 -0300

    Trabalhando com roles de usuários

--- libraryapi/src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.example.libraryapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,17 +19,23 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        return http
-//                .csrf(AbstractHttpConfigurer::disable)
-//                .formLogin(configure -> {
-//                    configure.loginPage("/login").permitAll();
-//                })
-//                .authorizeHttpRequests(authorize -> {
-//                    authorize.anyRequest().authenticated();
-//                }).build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(configure -> {
+                    configure.loginPage("/login");
+                })
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.GET, "/autores/**").hasAnyRole("ADMIN", "USER");
+                    authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
+                    authorize.anyRequest().authenticated();
+                }).build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
