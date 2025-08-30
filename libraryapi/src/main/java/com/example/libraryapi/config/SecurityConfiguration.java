45373444ee02122ede9fd2a7917eb88f47de0a2b
brunohuguenin <brunohugueninn@gmail.com>
2025-08-30 18:34:01 -0300

    Configurando autenticação do Google com formulário próprio

--- libraryapi/src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
+++ libraryapi/src/main/java/com/example/libraryapi/config/SecurityConfiguration.java
@@ -30,17 +30,18 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .httpBasic(Customizer.withDefaults())
-//                .formLogin(configure -> {
-//                    configure.loginPage("/login");
-//                })
-                .formLogin(Customizer.withDefaults())
+                .formLogin(configure -> {
+                    configure.loginPage("/login");
+                })
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/login/**").permitAll();
                     authorize.requestMatchers( HttpMethod.POST,"/usuarios/**").permitAll();
                     authorize.anyRequest().authenticated();
                 })
                 .oauth2Login(oauth2 -> {
-                    oauth2.successHandler(successHandler);
+                    oauth2
+                            .loginPage("/login")
+                            .successHandler(successHandler);
                 })
                 .build();
     }
