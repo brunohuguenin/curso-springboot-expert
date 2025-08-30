70632d2f0292d449064763a88edebfd0bc7cdab0
brunohuguenin <brunohugueninn@gmail.com>
2025-08-30 14:08:48 -0300

    Removendo application.yml do controle de versão

---
+++ libraryapi/src/main/java/com/example/libraryapi/securiy/CustomAuthentication.java
@@ -0,0 +1,50 @@
+package com.example.libraryapi.securiy;
+
+import com.example.libraryapi.model.Usuario;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class CustomAuthentication implements Authentication {
+
+    private final Usuario usuario;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return false;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+
+    }
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
