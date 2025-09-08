dd09979b6b706de980f8bbc086eb1e2bb0e6f0f1
brunohuguenin <brunohugueninn@gmail.com>
2025-09-08 20:49:50 -0300

    Configurações globais para clients

--- libraryapi/src/main/java/com/example/libraryapi/securiy/CustomRegisteredClientRepository.java
+++ libraryapi/src/main/java/com/example/libraryapi/securiy/CustomRegisteredClientRepository.java
@@ -6,6 +6,8 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
 import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.stereotype.Component;
 
 
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Component;
 public class CustomRegisteredClientRepository implements RegisteredClientRepository {
 
     private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
 
     @Override
     public void save(RegisteredClient registeredClient) {
@@ -40,6 +44,8 @@ public class CustomRegisteredClientRepository implements RegisteredClientReposit
                 .scope(client.getScope())
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
                 .build();
     }
 }
